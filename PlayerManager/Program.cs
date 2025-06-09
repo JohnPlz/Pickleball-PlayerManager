57f603e8716a02d61a0818bdaa389bd8e20c0399
Ioannis Michail <61376515+JohnPlz@users.noreply.github.com>
2025-06-09 21:21:45 +0200

    add class Player (#1)
    * add class Player
    
    * ich habe die class Player deprate gemacht
    
    ---------
    
    Co-authored-by: dn220986tev <dn220986tev1@gmail.com>

--- PlayerManager/Program.cs
+++ PlayerManager/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Pickleball_PlayerManager
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var player = new Player()
+            {
+                Name = "Olena",
+                Age = 38,
+                Rating = 3.5
+
+            };
+
+            player.PrintInfo();
+
+        }   
+    } 
+}

f9f13c953cad87c6f25de3915247f0691d112990
dn220986tev <dn220986tev1@gmail.com>
2025-06-09 14:48:15 +0300

    add class Player

--- PlayerManager/Program.cs
+++ PlayerManager/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Pickleball_PlayerManager
+{
+    public class Player
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public double Rating { get; set; }
+
+        public void PrintInfo()
+        {
+            System.Console.WriteLine($"Name: {Name}, Age: {Age}, Rating: {Rating}");
+        }
+    
+        public static void Main(string[] args)
+        {
+            var player = new Player()
+            {
+                Name = "Olena",
+                Age = 38,
+                Rating = 3.5
+
+            };
+
+            player.PrintInfo();
+
+        }   
+    } 
+}

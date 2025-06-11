34e9aa24f7077c4296113df0b5bcbc0296c9f4e4
dn220986tev <dn220986tev1@gmail.com>
2025-06-11 12:49:52 +0300

    class Match bearbeitet und Spieler eintragen

--- PlayerManager/Player.cs
+++ PlayerManager/Player.cs
@@ -6,11 +6,18 @@ namespace Pickleball_PlayerManager
         public string? Name { get; set; }
         public int Age { get; set; }
         public double Rating { get; set; }
-
+        public void InputInfoPlayer()
+        {
+            Console.Write("Enter the player´s name: ");
+            Name = Console.ReadLine();
+            Console.Write("Enter the player´s age: ");
+            int Age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the player´s Rating: ");
+            double Rating = Convert.ToDouble(Console.ReadLine());
+        }
         public void PrintInfo()
         {
-            System.Console.WriteLine(Name);
-            //System.Console.WriteLine($"Name: {Name}, Age: {Age}, Rating: {Rating}");
+         System.Console.WriteLine($"Name: {Name}, Age: {Age}, Rating: {Rating}");
         }
 
     
--- PlayerManager/Program.cs
+++ PlayerManager/Program.cs
@@ -6,49 +6,13 @@ namespace Pickleball_PlayerManager
     {
         public static void Main(string[] args)
         {
-            /*var player = new Player()
-            {
-                Name = "Olena",
-                Age = 38,
-                Rating = 3.5
-
-            };
-            */
-            List<Player> players = new List<Player>();
-
             Player player1 = new Player();
-            player1.Name = "Olena";
-            player1.Age = 38;
-            player1.Rating = 3.5;
-
+            player1.InputInfoPlayer();
             Player player2 = new Player();
-            player2.Name = "Anastasia";
-            player2.Age = 27;
-            player2.Rating = 3.5;
-
-            Player player3 = new Player();
-            player3.Name = "Alex";
-            player3.Age = 28;
-            player3.Rating = 3.0;
-
-            Player player4 = new Player();
-            player4.Name = "Tom";
-            player4.Age = 40;
-            player4.Rating = 3.0;
-
-
-            players.Add(player1);
-            players.Add(player2);
-            players.Add(player3);
-            players.Add(player4);
-
-
-            foreach (Player player in players)
-            {
-                player.PrintInfo();
-            }
-
-            Match match = new Match();
+            player2.InputInfoPlayer();
+            System.Console.WriteLine("Players: ");
+            player1.PrintInfo();
+            player2.PrintInfo();
 
         }   
     } 
